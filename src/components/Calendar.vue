21f497d289b0f863b86dc9bfe41f0326448e4889
Александр Бабкин <babkin@dinamika-avia.ru>
2021-12-01 19:39:56 +0300

    Добавил компонент Calendar

---
+++ src/components/Calendar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="calendar">
+    <h3>Каледнарь</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
