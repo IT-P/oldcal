7567f9bb0f152520d8cecee373d9ff3655a71f49
Александр Бабкин <babkin@dinamika-avia.ru>
2021-12-01 19:19:06 +0300

    Добавил Валькирию

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Old calendar logo" src="./assets/simbol-valkirija.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
