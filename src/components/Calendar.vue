cdb66fbd168cdcc8e79dc972ea48f378c28cdeae
Александр Бабкин <babkin@dinamika-avia.ru>
2021-12-14 16:30:51 +0300

    Добавил внешний css, решил месяцы делать отдельно

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,135 +1,418 @@
 <template>
-  <div class="calendar">
-    <div class="container">
-      <h3>Календарь</h3>
+  <div class="container">
+    <div align="center">
+      <h1>Коляды Даръ 7530</h1>
+      <div style="page-break-after: always"></div>
+      <div>&nbsp;</div>
+      <table border="0" class="zagolovok" width="800px">
+        <tr>
+          <th class="bok" width="33%">
+            Лесной Орёл <br />
+            10-е Лѣто 53-го Круга<br />
+            на Круголѣте Числобога
+          </th>
+          <th>
+            <span class="title red" width="34%">ЭПОХА ВОЛКА</span><br /><span
+              class="title"
+            >
+              Рамхатъ </span
+            ><br /><span class="subtitle"> Месяц Божественнаго Начала </span>
+          </th>
+          <th class="bok" width="33%">
+            Лѣто 13030<br />отъ Великай Стужи<br />на Круголѣте Числобога
+          </th>
+        </tr>
+      </table>
+      <table class="mesyac">
+        <tr>
+          <td class="mesyac nazvaniya_dney">Понедѣльникъ</td>
+          <td class="mesyac red">1</td>
+          <td class="mesyac grig_mini">
+            Сентябрь<br />2021<br />20&nbsp;Пн
+            <hr />
+            21&nbsp;Вт
+          </td>
+          <td class="mesyac">10</td>
+          <td class="mesyac grig">
+            29&nbsp;Ср
+            <hr />
+            30&nbsp;Чт
+          </td>
+          <td class="mesyac red">19</td>
+          <td class="mesyac grig">
+            08&nbsp;Пт
+            <hr />
+            09&nbsp;Сб
+          </td>
+          <td class="mesyac post">28</td>
+          <td class="mesyac grig">
+            17&nbsp;Вс
+            <hr />
+            18&nbsp;Пн
+          </td>
+          <td class="mesyac">37</td>
+          <td class="mesyac grig">
+            26&nbsp;Вт
+            <hr />
+            27&nbsp;Ср
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney">Вторникъ</td>
+          <td class="mesyac">2</td>
+          <td class="mesyac grig">
+            21&nbsp;Вт
+            <hr />
+            22&nbsp;Ср
+          </td>
+          <td class="mesyac red">11</td>
+          <td class="mesyac grig_mini">
+            Октябрь<br />2021<br />30&nbsp;Чт
+            <hr />
+            01&nbsp;Пт
+          </td>
+          <td class="mesyac">20</td>
+          <td class="mesyac grig">
+            09&nbsp;Сб
+            <hr />
+            10&nbsp;Вс
+          </td>
+          <td class="mesyac post">29</td>
+          <td class="mesyac grig">
+            18&nbsp;Пн
+            <hr />
+            19&nbsp;Вт
+          </td>
+          <td class="mesyac red roditelskie_dni">38</td>
+          <td class="mesyac grig">
+            27&nbsp;Ср
+            <hr />
+            28&nbsp;Чт
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney">Тритейникъ</td>
+          <td class="mesyac">3</td>
+          <td class="mesyac grig">
+            22&nbsp;Ср
+            <hr />
+            23&nbsp;Чт
+          </td>
+          <td class="mesyac">12</td>
+          <td class="mesyac grig">
+            01&nbsp;Пт
+            <hr />
+            02&nbsp;Сб
+          </td>
+          <td class="mesyac">21</td>
+          <td class="mesyac grig">
+            10&nbsp;Вс
+            <hr />
+            11&nbsp;Пн
+          </td>
+          <td class="mesyac post">30</td>
+          <td class="mesyac grig">
+            19&nbsp;Вт
+            <hr />
+            20&nbsp;Ср
+          </td>
+          <td class="mesyac">39</td>
+          <td class="mesyac grig">
+            28&nbsp;Чт
+            <hr />
+            29&nbsp;Пт
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney">Четверикъ</td>
+          <td class="mesyac red">4</td>
+          <td class="mesyac grig">
+            23&nbsp;Чт
+            <hr />
+            24&nbsp;Пт
+          </td>
+          <td class="mesyac">13</td>
+          <td class="mesyac grig">
+            02&nbsp;Сб
+            <hr />
+            03&nbsp;Вс
+          </td>
+          <td class="mesyac red">22</td>
+          <td class="mesyac grig">
+            11&nbsp;Пн
+            <hr />
+            12&nbsp;Вт
+          </td>
+          <td class="mesyac red post">31</td>
+          <td class="mesyac grig">
+            20&nbsp;Ср
+            <hr />
+            21&nbsp;Чт
+          </td>
+          <td class="mesyac red dni_predkov">40</td>
+          <td class="mesyac grig">
+            29&nbsp;Пт
+            <hr />
+            30&nbsp;Сб
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney">Пятница</td>
+          <td class="mesyac">5</td>
+          <td class="mesyac grig">
+            24&nbsp;Пт
+            <hr />
+            25&nbsp;Сб
+          </td>
+          <td class="mesyac red roditelskie_dni">14</td>
+          <td class="mesyac grig">
+            03&nbsp;Вс
+            <hr />
+            04&nbsp;Пн
+          </td>
+          <td class="mesyac post">23</td>
+          <td class="mesyac grig">
+            12&nbsp;Вт
+            <hr />
+            13&nbsp;Ср
+          </td>
+          <td class="mesyac">32</td>
+          <td class="mesyac grig">
+            21&nbsp;Чт
+            <hr />
+            22&nbsp;Пт
+          </td>
+          <td class="mesyac">41</td>
+          <td class="mesyac grig">
+            30&nbsp;Сб
+            <hr />
+            31&nbsp;Вс
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney">Шестица</td>
+          <td class="mesyac red">6</td>
+          <td class="mesyac grig">
+            25&nbsp;Сб
+            <hr />
+            26&nbsp;Вс
+          </td>
+          <td class="mesyac">15</td>
+          <td class="mesyac grig">
+            04&nbsp;Пн
+            <hr />
+            05&nbsp;Вт
+          </td>
+          <td class="mesyac red dni_predkov post">24</td>
+          <td class="mesyac grig">
+            13&nbsp;Ср
+            <hr />
+            14&nbsp;Чт
+          </td>
+          <td class="mesyac">33</td>
+          <td class="mesyac grig">
+            22&nbsp;Пт
+            <hr />
+            23&nbsp;Сб
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney sedmica">Седьмица</td>
+          <td class="mesyac">7</td>
+          <td class="mesyac grig">
+            26&nbsp;Вс
+            <hr />
+            27&nbsp;Пн
+          </td>
+          <td class="mesyac red dni_predkov">16</td>
+          <td class="mesyac grig">
+            05&nbsp;Вт
+            <hr />
+            06&nbsp;Ср
+          </td>
+          <td class="mesyac post">25</td>
+          <td class="mesyac grig">
+            14&nbsp;Чт
+            <hr />
+            15&nbsp;Пт
+          </td>
+          <td class="mesyac">34</td>
+          <td class="mesyac grig">
+            23&nbsp;Сб
+            <hr />
+            24&nbsp;Вс
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney">Осьмица</td>
+          <td class="mesyac">8</td>
+          <td class="mesyac grig">
+            27&nbsp;Пн
+            <hr />
+            28&nbsp;Вт
+          </td>
+          <td class="mesyac">17</td>
+          <td class="mesyac grig">
+            06&nbsp;Ср
+            <hr />
+            07&nbsp;Чт
+          </td>
+          <td class="mesyac post">26</td>
+          <td class="mesyac grig">
+            15&nbsp;Пт
+            <hr />
+            16&nbsp;Сб
+          </td>
+          <td class="mesyac">35</td>
+          <td class="mesyac grig">
+            24&nbsp;Вс
+            <hr />
+            25&nbsp;Пн
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="mesyac nazvaniya_dney red">Недѣля</td>
+          <td class="mesyac red dni_predkov">9</td>
+          <td class="mesyac grig">
+            28&nbsp;Вт
+            <hr />
+            29&nbsp;Ср
+          </td>
+          <td class="mesyac">18</td>
+          <td class="mesyac grig">
+            07&nbsp;Чт
+            <hr />
+            08&nbsp;Пт
+          </td>
+          <td class="mesyac red roditelskie_dni post">27</td>
+          <td class="mesyac grig">
+            16&nbsp;Сб
+            <hr />
+            17&nbsp;Вс
+          </td>
+          <td class="mesyac red dni_predkov">36</td>
+          <td class="mesyac grig">
+            25&nbsp;Пн
+            <hr />
+            26&nbsp;Вт
+          </td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+          <td class="mesyac">&nbsp;</td>
+          <td class="mesyac grig">&nbsp;</td>
+        </tr>
+      </table>
+      <br />
+      <table border="0" class="legenda">
+        <tr>
+          <td class="dni_predkov">&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Дни Памяти и Почитания Предковъ</td>
+          <td>- 9, 16, 24, 36, 40</td>
+          <td>Рамхатъ</td>
+        </tr>
+        <tr>
+          <td class="roditelskie_dni">&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Родительские дни</td>
+          <td>- 14, 27, 38</td>
+          <td>Рамхатъ</td>
+        </tr>
+        <tr>
+          <td class="post">&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Святый ПОСТЪ</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="sedmica">&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>Однодневный ПОСТЪ</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+      <br />
+      <div class="red">Славяно-Арийckiя праzдниkи на месяц Рамхатъ</div>
+      <div class="spisok_prazdnikov">
+        <span style="white-space: nowrap"
+          ><span class="red">1</span> - НОВОЛѢТIЕ</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">4</span> - День Ивана Омiйскаго</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">6</span> - Праzдниkъ Урожая</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">9</span> - День Сергiя Странниkа</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">11</span> - День Ольги Премудрой</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">14</span> - День Великай Рассенiи</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">16</span> - День Василiя Буслаева</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">19</span> - День Харiя Мудраго</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">22</span> - День Вѣдъ</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">24</span> - День Трояна</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">27</span> - День Святибора</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">31</span> - День Моисiя Волхва</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">36</span> - День Сканда Ирiйскаго</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">38</span> - День БОГИНИ РОЖАНЫ</span
+        >
+        <span style="white-space: nowrap"
+          ><span class="red">40</span> - Дидовъ День</span
+        >
+      </div>
+      <br />
+      <br />
+      <div style="page-break-after: always"></div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Calendar'
-}
+  name: "Calendar",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .calendar {
-    font-family: "CyrillicOld";
-    color: rgb(3,77,124);
-    font-size: 20pt;
-  }
-
-  .container {
-    width: 900px;
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1,h2,h3,h4 {
-    color:rgb(238,29,31);
-  }
-
-  .title {
-    font-size: 20pt;
-  }
-
-  .subtitle {
-    font-size: 12pt;
-  }
-  .red{
-    color:rgb(238,29,31);
-  }
-  .sedmica {
-    background-color: rgb(246,255,175);
-  }
-  .dni_predkov {
-    background-color: rgb(255,125,124);
-  }
-  .roditelskie_dni {
-    background-color: rgb(110,246,136);
-  }
-  .post {
-    background-image: linear-gradient(45deg, rgb(118,214,251) 50%, transparent 50%);
-  }
-
-  .zhertvy_perunu {
-    background-color: rgb(255,32,28);
-  }
-
-  .zhatva {
-    background-color: rgb(83,27,147);
-  }
-
-  table.zagolovok {
-    font-size: 14pt;
-  }
-
-  table.zagolovok td.bok, th.bok {
-    color: rgb(238,29,31);	
-  }
-
-
-  table.mesyac {
-    border: 1px solid rgb(3,77,124);
-    border-collapse: collapse;
-    font-size: 20pt;
-  }
-
-  th.mesyac, td.mesyac {
-    border: 1px solid rgb(3,77,124);
-    padding: 5px;
-    margin: 5px;
-    width: 1.5em;
-    height: 2em;
-    border-right: 0px;
-    text-align: center;
-  }
-
-  .nazvaniya_dney {
-    text-align: center;
-  }
-
-  hr {
-    border-top: 1px solid rgb(0,113,31);
-    border-bottom: 0px;
-    /*height: 1px;*/
-    margin: 0;
-    /*padding-bottom: 1px;*/
-    border-collapse: collapse;
-  }
-
-  td.grig {
-    font-family: "Arial";
-    font-size: 9pt;
-    border-left: 0px;
-    color:rgb(0,113,31);
-    text-align: center;
-    width: 4em;
-
-  }
-
-  td.grig_mini {
-    font-family: "Arial";
-    font-size: 7pt;
-    border-left: 0px;
-    color:rgb(0,113,31);
-    text-align: center;
-    width: 4em;
-
-  }
-
-  .spisok_prazdnikov {
-    font-size: 14pt;
-  }
-
-  table.legenda {
-    font-size: 14pt;
-  }
-
-  th.legenda,td.legenda {
-    white-space: nowrap;
-    border: 0px;
-    text-align: left;
-  }
 </style>
