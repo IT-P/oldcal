304f12efdc0c57484c75272b1372e231f9f2ac41
Александр Бабкин <babkin@dinamika-avia.ru>
2021-12-16 21:20:58 +0300

    Начал делать выбор текущей даты

---
+++ src/components/Ramhat.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <span class="title">Рамхатъ</span><br/>
+        <span class="subtitle">Месяц Божественнаго Начала</span>
+    </div>
+</template>
+<script>
+
+</script>
