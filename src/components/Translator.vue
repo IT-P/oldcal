7436ec78fa6fbe9f482fd2772ed853d5817d7f17
Александр Бабкин <_shurick@mail.ru>
2022-04-09 23:11:28 +0300

    first version of translate ready

--- src/components/Translator.vue
+++ src/components/Translator.vue
@@ -15,26 +15,29 @@
       <tr>
         <td align="right">Лето</td>
         <td>{{ LETO }}</td>
+        <td>{{ NAZVANIYE_LETA }}</td>
       </tr>
       <tr>
         <td align="right">Месяц</td>
-        <td>{{ Mesyac }}</td>
+        <td>{{ MESYAC }}</td>
+        <td>{{ MESYAC_OPISANIE }}</td>
       </tr>
       <tr>
         <td align="right">День</td>
-        <td>{{ Den }}</td>
+        <td>{{ DEN }}</td>
+        <td></td>
       </tr>
       <tr>
         <td align="right">Лето в круге жизни</td>
-        <td>{{ LETO_V_KRUGE_ZHIZNI }}</td>
+        <td colspan="2">{{ LETO_V_KRUGE_ZHIZNI }}</td>
       </tr>
       <tr>
         <td align="right">Лето в круге лет</td>
-        <td>{{ LETO_V_KRUGE_LET }}</td>
+        <td colspan="2">{{ LETO_V_KRUGE_LET }}</td>
       </tr>
     </table>
   </div>
-  <!-- <div>{{ Den }} {{ Mesyac }} {{ LETO }}</div> -->
+  <!-- <div>{{ MESYAC_NACHALO }} {{ date }} {{ DEN }}</div> -->
 </template>
 
 <script>
@@ -48,10 +51,13 @@ export default {
       max_date: new Date("2156-09-22"),
       grig_date_string: "",
       LETO: "",
+      NAZVANIYE_LETA: "",
       LETO_V_KRUGE_ZHIZNI: 0,
       LETO_V_KRUGE_LET: 0,
-      Mesyac: "",
-      Den: "",
+      MESYAC: "",
+      MESYAC_OPISANIE: "",
+      MESYAC_NACHALO: "",
+      DEN: "",
     };
   },
   created: function () {
@@ -81,11 +87,14 @@ export default {
     },
     translate: function () {
       var current_grig_date = new Date(this.grig_date_string);
+      this.date = current_grig_date;
+
       if (current_grig_date < this.min_date) {
         current_grig_date = this.min_date;
         this.date = this.min_date;
         this.dateToString();
       }
+
       if (current_grig_date > this.max_date) {
         current_grig_date = this.max_date;
         this.date = this.max_date;
@@ -143,6 +152,66 @@ export default {
       if (this.LETO_V_KRUGE_LET === 0) {
         this.LETO_V_KRUGE_LET = 16;
       }
+      this.NAZVANIYE_LETA =
+        store.state.NazvaniyaLet.NAZVANIYA_LET[this.LETO_V_KRUGE_ZHIZNI - 1];
+      this.getMesyac();
+    },
+    getMesyac: function () {
+      var nach_mes = Array();
+
+      for (let i = 0; i < 9; i++) {
+        var tmp_str_array = store.state.GrigDates.leto_grig[this.LETO_V_KRUGE_LET][
+          i
+        ].split(".");
+        nach_mes[i] = new Date(
+          this.date.getFullYear() + "-" + tmp_str_array[1] + "-" + tmp_str_array[0]
+        );
+        nach_mes[i].setDate(nach_mes[i].getDate() - 1);
+      }
+
+      if (this.date >= nach_mes[0] && this.date < nach_mes[1]) {
+        this.MESYAC = store.state.Ramhat.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Ramhat.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[0]) / (1000 * 60 * 60 * 24) + 1;
+      } else if (this.date >= nach_mes[1] && this.date < nach_mes[2]) {
+        this.MESYAC = store.state.Ailet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Ailet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[1]) / (1000 * 60 * 60 * 24) + 1;
+      } else if (this.date >= nach_mes[2]) {
+        this.MESYAC = store.state.Beilet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Beilet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[2]) / (1000 * 60 * 60 * 24) + 1;
+      } else if (this.date < nach_mes[3]) {
+        this.MESYAC = store.state.Beilet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Beilet.MESYAC_OPISANIE;
+        this.DEN = 42 - (nach_mes[3] - this.date) / (1000 * 60 * 60 * 24);
+      } else if (this.date >= nach_mes[3] && this.date < nach_mes[4]) {
+        this.MESYAC = store.state.Geilet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Geilet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[3]) / (1000 * 60 * 60 * 24) + 1;
+      } else if (this.date >= nach_mes[4] && this.date < nach_mes[5]) {
+        this.MESYAC = store.state.Dailet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Dailet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[4]) / (1000 * 60 * 60 * 24) + 1;
+        // this.MESYAC_NACHALO = nach_mes[4];
+      } else if (this.date >= nach_mes[5] && this.date < nach_mes[6]) {
+        this.MESYAC = store.state.Elet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Elet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[5]) / (1000 * 60 * 60 * 24) + 1;
+      } else if (this.date >= nach_mes[6] && this.date < nach_mes[7]) {
+        this.MESYAC = store.state.Veilet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Veilet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[6]) / (1000 * 60 * 60 * 24) + 1;
+      } else if (this.date >= nach_mes[7] && this.date < nach_mes[8]) {
+        this.MESYAC = store.state.Heilet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Heilet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[7]) / (1000 * 60 * 60 * 24) + 1;
+      } else if (this.date >= nach_mes[8]) {
+        this.MESYAC = store.state.Tailet.MESYAC;
+        this.MESYAC_OPISANIE = store.state.Tailet.MESYAC_OPISANIE;
+        this.DEN = (this.date - nach_mes[8]) / (1000 * 60 * 60 * 24) + 1;
+      }
+      // var current_grig_date = new Date(this.grig_date_string);
     },
   },
 };
